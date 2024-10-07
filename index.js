24f588495acb937ace0d09918d63fdd4506c9af9
aryansingla <singlaaryan2001@gmail.com>
2024-10-07 18:01:16 +0530

    server.js name change to index.js

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ import cors from 'cors';
 import passport from 'passport';
 import authRoutes from './routes/auth.js';
 import interviewRoutes from './routes/interviewRoutes.js';
+import dotenv from 'dotenv';
+
+
+dotenv.config({ path: '.env.local' });
 
 const app = express();
 
@@ -13,7 +17,7 @@ app.use(cors());
 app.use(passport.initialize());
 
 // MongoDB Connection
-mongoose.connect('mongodb://localhost:27017/usersDB', {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
@@ -26,3 +30,4 @@ app.use('/api', interviewRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
